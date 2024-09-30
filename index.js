44cb7c598d5cfa09d5e0bdb7e63701291bd292e7
ShivamSh0608 <Shivam.sharma@rhope.in>
2024-09-30 13:16:52 +0530

    small change

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ const tradeRoutes = require('./routes/tradeRoutes');
 
 const app = express();
 
-// Connect to MongoDB
 connectDB();
 
-// Middleware to handle JSON requests
 app.use(express.json());
 
-// Use trade routes
 app.use('/api/trades', tradeRoutes);
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
